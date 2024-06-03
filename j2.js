a5f339e326af2dca2897aa54bc9c41f37d53d588
nathalyamarques <nathalyamarques2016@gmail.com>
2024-06-03 19:45:49 -0300

    ultimas atividades

---
+++ j2.js
@@ -0,0 +1,26 @@
+// aula Js
+
+function oi()
+{
+    console.log(valor.value);
+}
+
+function mais(){
+    console.log(valor.value.toUpperCase());
+}
+
+function primeira (){
+    var texto = valor.value.toLowerCase();
+    texto = texto[0].toUpperCase() + texto.slice(1);
+    console.log(texto);
+}
+
+function contagem() {
+    var numero = valor.value;
+
+    while (numero >=0){
+        var contagem = numero;
+        console.log(contagem);
+        numero = numero - 1;
+    }
+}
